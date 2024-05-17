b08cc765cff39424dd8001642788a42b358ecf67
RegroupForces <161395559+RegroupForces@users.noreply.github.com>
2024-05-17 10:16:53 +1000

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -9,10 +9,15 @@ def list_to_number(lst: list) -> int:
 
     Example:
     [8,5,3,1] should be 8531, which is also:
+    
     8*1000 + 5*100 + 3*10 + 1*1
-    also:
-    8*10^3 + 5*10^2 + 3*10^1 + 1*10^0, and this code breaks the list into this format and calculate it.
+    
+    also equal to:
+    
+    8*10^3 + 5*10^2 + 3*10^1 + 1*10^0
+    and this code breaks the list into the above format and calculate it.
 
+    This assumes there are no negative numbers there.
     """
     number = 0
     for i, digit in enumerate(lst):
@@ -20,7 +25,7 @@ def list_to_number(lst: list) -> int:
     return number
 
 if __name__ == "__main__":
-    """This is testcases, assuming all intergers in lst are positive."""
+    """These are testcases, assuming all intergers in lst are positive."""
     print(list_to_number([8,3,5,1]))
     print(list_to_number([1,4,6,2,5,2,1]))
     print(list_to_number([1]))
